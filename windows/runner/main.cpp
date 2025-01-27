d8c62bedf731eba7beb7928133e95830d0350443
Júlia Patricia Wolschick <juliapwols@gmail.com>
2025-01-27 17:07:34 -0300

    Windows release

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -26,8 +26,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
-  if (!window.Create(L"julia_conversion_tool", origin, size)) {
+  Win32Window::Size size(1200, 800);
+  if (!window.Create(L"Julia Conversion Tool", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
